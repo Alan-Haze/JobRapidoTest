baec6edd1721ccc70dc12d5bf45512e52c223d9e
Alan Liu <103892610+Alan-Haze@users.noreply.github.com>
2022-10-06 16:04:09 +0200

    PointSegmentCLass

---
+++ demo/src/main/java/com/example/demo/Class/Point.java
@@ -0,0 +1,27 @@
+package com.example.demo.Class;
+
+public class Point {
+    Integer x;
+    Integer y;
+
+    public Point(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
+}
---
+++ demo/src/main/java/com/example/demo/Class/Segment.java
@@ -0,0 +1,29 @@
+package com.example.demo.Class;
+
+import java.awt.*;
+
+public class Segment {
+    Point point1;
+    Point point2;
+
+    public Segment(Point point1, Point point2) {
+        this.point1 = point1;
+        this.point2 = point2;
+    }
+
+    public Point getPoint1() {
+        return point1;
+    }
+
+    public void setPoint1(Point point1) {
+        this.point1 = point1;
+    }
+
+    public Point getPoint2() {
+        return point2;
+    }
+
+    public void setPoint2(Point point2) {
+        this.point2 = point2;
+    }
+}
